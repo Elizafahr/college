543fd317bc66a17b8d2180a36fbfd4ef17c2dc2d
Elizafahr <elizafahr@gmail.com>
2024-05-29 23:02:24 +0500

    \

--- collegeThirdYear/bodnya/05.02(code)/TipeScript/ts/college_second-pack-of-tasks/src/tasks/sixth.ts
+++ collegeThirdYear/bodnya/05.02(code)/TipeScript/ts/college_second-pack-of-tasks/src/tasks/sixth.ts
@@ -1,32 +1,32 @@
 import "../style.css";
-
+ 
 //Разработайте сценарий для веб-страницы, который переведет в верхний регистр первую букву каждого слова, 
 //следующего за точкой.
 document.querySelector<HTMLDivElement>("#sixth")!.innerHTML = `
   <div class="Sixth">
   <h1>Шестое задание</h1> 
   <p>(начните вводить текст) Разработайте сценарий для веб-страницы, который переведет в верхний регистр первую букву каждого слова, следующего за точкой.</p>
-    <textarea id="textareaSec"></textarea>
+    <textarea id="textareaSix"></textarea>
      <ul id="sentences"></ul>
   </div>
-`;
+`; 
 
-const textareaSec: HTMLTextAreaElement = document.getElementById('textareaSec') as HTMLTextAreaElement;
-textareaSec.oninput = function () {
-  splitTextSec();
+const textareaSix: HTMLTextAreaElement = document.getElementById('textareaSix') as HTMLTextAreaElement;
+textareaSix.oninput = function(){
+  splitTextSix();
 }
 
-function splitTextSec(): void {
-  const textareaValue = textareaSec.value;
-  let arr: Array<string> = [];
-  var splitted = textareaValue.split(".");
+function splitTextSix(): void {
+  const textareaValue = textareaSix.value;
+  let arr : Array<string> = [];
+  var splitted = textareaValue.split("."); 
   splitted.forEach(element => {
     arr.push(element.trim());
   });
-  outPut(arr);
+  out(arr);
 }
 
-function outPut(arr: Array<string>): void {
+function out(arr: Array<string>): void {
   const sentencesList = document.querySelector<HTMLUListElement>("#sentences")!;
   sentencesList.innerHTML = "";
   arr.forEach(element => {
